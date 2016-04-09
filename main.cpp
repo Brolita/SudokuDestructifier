0ad6ae81a0d330ab5251331319110c32d822a403
tkxxrf <tkingfaster@yahoo.com>
2016-04-09 16:35:24 -0400

    Update main.cpp
    added testing for printboard

--- main.cpp
+++ main.cpp
@@ -2,9 +2,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <cmath.h>
+#include <cmath>
+
+#include "Board.h"
 
 int main(int argc, char* argv[])
 {
-
+	Board b = Board(81);
+	b.printBoard();
 }
